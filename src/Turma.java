8ab04272a29df2237e4507b8b0163355b40b2bb2
Luís Antônio <luisantoniossouza@gmail.com>
2022-03-25 11:33:43 -0300

    constants added

--- src/Turma.java
+++ src/Turma.java
@@ -1,13 +1,23 @@
 public class Turma {
-  private int nivel, diaDaSemana, aulasMinistradas, pontosPorProva;
+  static final int MAXIMO_DE_ALUNOS = 20;
+  static final int NUMERO_DE_ATIVIDADES = 4;
+  static final int TOTAL_DE_PONTOS = 100;
+  static final int NUMERO_DE_AULAS = 20;
+  private int nivel, diaDaSemana;
   private String turno;
 
+  Turma() {
+    this.setup(0, 0, " ");
+  }
 
   Turma(int nivel, int diaDaSemana, String turno) {
+    this.setup(nivel, diaDaSemana, turno);
+  }
+
+  private void setup(int nivel, int diaDaSemana, String turno) {
     this.definirNivel(nivel);
     this.definirDia(diaDaSemana);
     this.definirTurno(turno);
-
   }
 
   public void definirNivel(int nivel) {
